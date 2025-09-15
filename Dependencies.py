fd8ed4905ae1dd374440af635519f3d76a0da104
Amaan suheab <134421931+amaansuheab@users.noreply.github.com>
2025-09-15 18:05:33 +0530

    Update and rename Sales Predictions to Dependencies.py

---
+++ Dependencies.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+    ],
+)
